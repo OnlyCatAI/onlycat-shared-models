43c1f141ac96ea483ab979b4e605d74bf1f5900d
Martin Rosinski <martin@zone-mr.net>
2025-12-16 10:25:34 +0000

    update EventAdminTag to be class based

--- EventAdminTag.ts
+++ EventAdminTag.ts
@@ -1,8 +1,22 @@
-export interface EventAdminTag {
+export class EventAdminTag {
     deviceId: string;
     eventId: number;
     tag: string;
     taggedByUserId: number | null;
     taggedAt: Date;
-}
+    [key: string]: any;
 
+    constructor(initObj: Partial<EventAdminTag> & Record<string, any>) {
+        this.deviceId = initObj.deviceId!;
+        this.eventId = initObj.eventId!;
+        this.tag = initObj.tag!;
+        this.taggedByUserId = initObj.taggedByUserId ?? null;
+        this.taggedAt = initObj.taggedAt ? new Date(initObj.taggedAt) : new Date();
+
+        for (const key in initObj) {
+            if (initObj.hasOwnProperty(key) && !this.hasOwnProperty(key)) {
+                this[key] = initObj[key];
+            }
+        }
+    }
+}
