fbe913f06737c860a5c68a70d5c6f209683fa37f
Martin Rosinski <martin@zone-mr.net>
2026-02-17 10:26:36 +0000

    add EventSummary / SubEvent models
    This is a work in progress.
    
    It’s part of an emerging mechanism to split events into a timeline of what happened within each event, including classifying feline actions into PEEK/TRANSIT/DENY/BREACH

---
+++ EventSummary.ts
@@ -0,0 +1,17 @@
+export type SubEventDirection = "INWARD" | "OUTWARD";
+export type SubEventAction = "PEEK" | "TRANSIT" | "DENY" | "BREACH";
+
+export interface SubEvent {
+    startFrameIndex: number;
+    endFrameIndex: number;
+    rfidCode: string | null;
+    direction: SubEventDirection;
+    action: SubEventAction;
+}
+
+export interface EventSummary {
+    deviceId: string;
+    eventId: number;
+    frameCount: number;
+    subevents: SubEvent[];
+}
