892c85b1285e7b741a94ac0ab95da96398234083
Martin Rosinski <martin@zone-mr.net>
2026-06-29 10:25:42 +0100

    Add training snapshot criteria model

--- TrainingSnapshot.ts
+++ TrainingSnapshot.ts
@@ -1,5 +1,12 @@
 import { Snapshot } from "./Snapshot";
 
+export interface TrainingSnapshotCriteria {
+    deviceId?: string;
+    label?: string | null;
+    cameraClarity?: number;
+    hasCameraClarity?: boolean;
+}
+
 export class TrainingSnapshot extends Snapshot {
     label?: string;
     cameraClarity?: number;
